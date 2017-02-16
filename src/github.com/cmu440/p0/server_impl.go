73931252185ef03f1d7a2bd5804ce7b59ec6af04
altkatz <liqi8822@gmail.com>
2017-02-16 19:12:53 +0800

    impl1

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,41 @@
 
 package p0
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
 type keyValueServer struct {
-    // TODO: implement this!
+	// TODO: implement this!
+	listener net.Listener
 }
+
+var pServer *keyValueServer
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New() KeyValueServer {
-    // TODO: implement this!
-    return nil
+	// TODO: implement this!
+	pServer = &keyValueServer{}
+	return pServer
 }
 
 func (kvs *keyValueServer) Start(port int) error {
-    // TODO: implement this!
-    return nil
+	// TODO: implement this!
+	var err error
+	kvs.listener, err = net.Listen("tcp", ":"+strconv.Itoa(port))
+	fmt.Println("start error", err)
+	return err
 }
 
 func (kvs *keyValueServer) Close() {
-    // TODO: implement this!
+	// TODO: implement this!
 }
 
 func (kvs *keyValueServer) Count() int {
-    // TODO: implement this!
-    return -1
+	// TODO: implement this!
+	return -1
 }
 
 // TODO: add additional methods/functions below!
